da949a92e6aa80cfa3650616043adc3fa2d01745
G1Joshi <joshijeevan98@gmail.com>
2021-05-25 21:11:19 +0530

    add theme data

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,13 +5,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xaabbccdd),
       appBar: AppBar(
-        backgroundColor: Color(0xaabbccdd),
         title: Text(
           'News 24x7',
           style: TextStyle(
-            color: Color(0xff304050),
+            color: Color(0xff708090),
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -25,18 +23,18 @@ class HomePage extends StatelessWidget {
         itemBuilder: (context, index) => ListTile(
           leading: Icon(
             Icons.flag,
-            color: Color(0xff405060),
+            color: Color(0xff778899),
           ),
           title: Text(
             Countries.countries[index].name,
             style: TextStyle(
-              color: Color(0xff405060),
+              color: Color(0xff778899),
               fontWeight: FontWeight.bold,
             ),
           ),
           trailing: Icon(
             Icons.double_arrow_rounded,
-            color: Color(0xff405060),
+            color: Color(0xff778899),
           ),
         ),
       ),
