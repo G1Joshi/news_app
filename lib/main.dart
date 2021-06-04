5035b99547cb3b20355fb1269ebf254cd57152fc
G1Joshi <joshijeevan98@gmail.com>
2021-06-04 08:04:49 +0530

    update theme data

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ class NewsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'News 24x7',
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.blueGrey,
         primaryColor: Color(0xff667788),
         scaffoldBackgroundColor: Color(0xffddeeff),
+        cardColor: Color(0xffd0e0f0),
         appBarTheme: AppBarTheme(
           backgroundColor: Color(0xffd0e0f0),
           iconTheme: IconThemeData(
@@ -27,13 +28,21 @@ class NewsApp extends StatelessWidget {
           size: 30,
         ),
         textTheme: TextTheme(
+          headline5: TextStyle(
+            color: Color(0xff708090),
+            fontWeight: FontWeight.bold,
+          ),
           headline6: TextStyle(
             color: Color(0xff667788),
             fontWeight: FontWeight.bold,
           ),
-          headline5: TextStyle(
-            color: Color(0xff708090),
-            fontWeight: FontWeight.bold,
+          bodyText1: TextStyle(
+            color: Color(0xff192939),
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText2: TextStyle(
+            color: Color(0xff192939),
+            fontWeight: FontWeight.w500,
           ),
         ),
       ),
@@ -42,6 +51,7 @@ class NewsApp extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
         primaryColor: Color(0xffaabbcc),
         scaffoldBackgroundColor: Color(0xff102030),
+        cardColor: Color(0xff192939),
         appBarTheme: AppBarTheme(
           backgroundColor: Color(0xff112233),
           iconTheme: IconThemeData(
@@ -53,13 +63,21 @@ class NewsApp extends StatelessWidget {
           size: 30,
         ),
         textTheme: TextTheme(
+          headline5: TextStyle(
+            color: Color(0xffa0b0c0),
+            fontWeight: FontWeight.bold,
+          ),
           headline6: TextStyle(
             color: Color(0xffaabbcc),
             fontWeight: FontWeight.bold,
           ),
-          headline5: TextStyle(
-            color: Color(0xffa0b0c0),
-            fontWeight: FontWeight.bold,
+          bodyText1: TextStyle(
+            color: Color(0xffd0e0f0),
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText2: TextStyle(
+            color: Color(0xffd0e0f0),
+            fontWeight: FontWeight.w500,
           ),
         ),
       ),
