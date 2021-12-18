9b7518c3795fa81adee5850a132d25fefd2f90f9
G1Joshi <joshijeevan98@gmail.com>
2021-12-18 22:25:46 +0530

    update app

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/views/home_page.dart';
+import 'package:flutter/services.dart';
+import 'package:news_app/config/sharedprefs.dart';
+import 'package:news_app/config/constants.dart';
+import 'package:news_app/views/loading.dart';
 
-void main() {
-  runApp(NewsApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Preferences.init();
+  runApp(const NewsApp());
 }
 
 class NewsApp extends StatelessWidget {
+  const NewsApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News 24x7',
-      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      title: 'News App',
       theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.blueGrey,
-        primaryColor: Color(0xff667788),
-        scaffoldBackgroundColor: Color(0xffddeeff),
-        cardColor: Color(0xffd0e0f0),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xffd0e0f0),
-          iconTheme: IconThemeData(
-            color: Color(0xff667788),
-          ),
+        fontFamily: 'Helvetica',
+        scaffoldBackgroundColor: secondaryColorLight,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: primaryColorLight,
+          foregroundColor: secondaryColorLight,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
-        iconTheme: IconThemeData(
-          color: Color(0xff667788),
-          size: 30,
-        ),
-        textTheme: TextTheme(
-          headline5: TextStyle(
-            color: Color(0xff708090),
-            fontWeight: FontWeight.bold,
-          ),
-          headline6: TextStyle(
-            color: Color(0xff667788),
-            fontWeight: FontWeight.bold,
-          ),
-          bodyText1: TextStyle(
-            color: Color(0xff192939),
-            fontWeight: FontWeight.w700,
-          ),
-          bodyText2: TextStyle(
-            color: Color(0xff192939),
-            fontWeight: FontWeight.w500,
-          ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: primaryColorDark,
+          secondary: primaryColorLight,
         ),
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blueGrey,
-        primaryColor: Color(0xffaabbcc),
-        scaffoldBackgroundColor: Color(0xff102030),
-        cardColor: Color(0xff192939),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xff112233),
-          iconTheme: IconThemeData(
-            color: Color(0xffaabbcc),
-          ),
-        ),
-        iconTheme: IconThemeData(
-          color: Color(0xffaabbcc),
-          size: 30,
-        ),
-        textTheme: TextTheme(
-          headline5: TextStyle(
-            color: Color(0xffa0b0c0),
-            fontWeight: FontWeight.bold,
-          ),
-          headline6: TextStyle(
-            color: Color(0xffaabbcc),
-            fontWeight: FontWeight.bold,
-          ),
-          bodyText1: TextStyle(
-            color: Color(0xffd0e0f0),
-            fontWeight: FontWeight.w700,
-          ),
-          bodyText2: TextStyle(
-            color: Color(0xffd0e0f0),
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ),
-      home: HomePage(),
+      home: const Loading(),
     );
   }
 }
