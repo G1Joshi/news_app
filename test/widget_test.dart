edf91faf0b6c4d1f044811db3bdbfc28dd5865ae
G1Joshi <joshijeevan98@gmail.com>
2021-12-18 22:23:05 +0530

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,9 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:news_app/main.dart';
 
 void main() {
-  testWidgets('News test', (WidgetTester tester) async {
-    await tester.pumpWidget(NewsApp());
-
-    expect(find.text('News 24x7'), findsOneWidget);
+  testWidgets('test', (WidgetTester tester) async {
+    await tester.pumpWidget(const NewsApp());
   });
 }
