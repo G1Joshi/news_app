caa4afc37a9788d62c19968a6eebc04a3057f018
G1Joshi <joshijeevan98@gmail.com>
2021-12-18 22:24:45 +0530

    add models

---
+++ lib/data/models/country_model.dart
@@ -0,0 +1,228 @@
+class Country {
+  Country({
+    required this.code,
+    required this.name,
+  });
+
+  final String code;
+  final String name;
+
+  static final List<Country> countries = [
+    Country(
+      code: "ae",
+      name: "Arab Emirates",
+    ),
+    Country(
+      code: "ar",
+      name: "Argentina",
+    ),
+    Country(
+      code: "at",
+      name: "Austria",
+    ),
+    Country(
+      code: "au",
+      name: "Australia ",
+    ),
+    Country(
+      code: "be",
+      name: "Belgium",
+    ),
+    Country(
+      code: "bg",
+      name: "Bulgaria",
+    ),
+    Country(
+      code: "br",
+      name: "Brazil",
+    ),
+    Country(
+      code: "ca",
+      name: "Canada",
+    ),
+    Country(
+      code: "ch",
+      name: "Switzerland",
+    ),
+    Country(
+      code: "cn",
+      name: "China",
+    ),
+    Country(
+      code: "co",
+      name: "Colombia",
+    ),
+    Country(
+      code: "cu",
+      name: "Cuba",
+    ),
+    Country(
+      code: "cz",
+      name: "Czechia ",
+    ),
+    Country(
+      code: "de",
+      name: "Germany",
+    ),
+    Country(
+      code: "eg",
+      name: "Egypt",
+    ),
+    Country(
+      code: "fr",
+      name: "France ",
+    ),
+    Country(
+      code: "gb",
+      name: "Great Britain",
+    ),
+    Country(
+      code: "gr",
+      name: "Greece",
+    ),
+    Country(
+      code: "hk",
+      name: "Hong Kong",
+    ),
+    Country(
+      code: "hu",
+      name: "Hungary",
+    ),
+    Country(
+      code: "id",
+      name: "Indonesia",
+    ),
+    Country(
+      code: "ie",
+      name: "Ireland",
+    ),
+    Country(
+      code: "il",
+      name: "Israel",
+    ),
+    Country(
+      code: "in",
+      name: "India",
+    ),
+    Country(
+      code: "it",
+      name: "Italy",
+    ),
+    Country(
+      code: "jp",
+      name: "Japan",
+    ),
+    Country(
+      code: "kr",
+      name: "Korea",
+    ),
+    Country(
+      code: "lt",
+      name: "Lithuania",
+    ),
+    Country(
+      code: "lv",
+      name: "Latvia",
+    ),
+    Country(
+      code: "ma",
+      name: "Morocco",
+    ),
+    Country(
+      code: "mx",
+      name: "Mexico",
+    ),
+    Country(
+      code: "my",
+      name: "Malaysia",
+    ),
+    Country(
+      code: "ng",
+      name: "Nigeria",
+    ),
+    Country(
+      code: "nl",
+      name: "Netherlands",
+    ),
+    Country(
+      code: "no",
+      name: "Norway",
+    ),
+    Country(
+      code: "nz",
+      name: "New Zealand",
+    ),
+    Country(
+      code: "ph",
+      name: "Philippines",
+    ),
+    Country(
+      code: "pl",
+      name: "Poland",
+    ),
+    Country(
+      code: "pt",
+      name: "Portugal",
+    ),
+    Country(
+      code: "ro",
+      name: "Romania",
+    ),
+    Country(
+      code: "rs",
+      name: "Serbia",
+    ),
+    Country(
+      code: "ru",
+      name: "Russian Federation",
+    ),
+    Country(
+      code: "sa",
+      name: "Saudi Arabia",
+    ),
+    Country(
+      code: "se",
+      name: "Sweden",
+    ),
+    Country(
+      code: "sg",
+      name: "Singapore",
+    ),
+    Country(
+      code: "si",
+      name: "Slovenia",
+    ),
+    Country(
+      code: "sk",
+      name: "Slovakia",
+    ),
+    Country(
+      code: "th",
+      name: "Thailand",
+    ),
+    Country(
+      code: "tr",
+      name: "Turkey",
+    ),
+    Country(
+      code: "tw",
+      name: "Taiwan",
+    ),
+    Country(
+      code: "ua",
+      name: "Ukraine",
+    ),
+    Country(
+      code: "us",
+      name: "United States",
+    ),
+    Country(
+      code: "ve",
+      name: "Venezuela",
+    ),
+    Country(
+      code: "za",
+      name: "South Africa",
+    ),
+  ];
+}
