6ec9784c15f35b7e12ff81a2194e63807c8b0eee
G1Joshi <joshijeevan98@gmail.com>
2021-12-18 22:23:32 +0530

    add config

---
+++ lib/config/sharedprefs.dart
@@ -0,0 +1,33 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Preferences {
+  static late SharedPreferences preference;
+
+  static Future init() async {
+    preference = await SharedPreferences.getInstance();
+  }
+
+  static Future setCountry(String country) async {
+    await preference.setString("country", country);
+  }
+
+  static String getCountry() {
+    return preference.getString("country") ?? "India";
+  }
+
+  static Future setCountryCode(String countryCode) async {
+    await preference.setString("countryCode", countryCode);
+  }
+
+  static String getCountryCode() {
+    return preference.getString("countryCode") ?? "in";
+  }
+
+  static Future setSortBy(String sortBy) async {
+    await preference.setString("sortBy", sortBy);
+  }
+
+  static String getSortBy() {
+    return preference.getString("sortBy") ?? "publishedAt";
+  }
+}
