0a5d0a178158225afdada9cd99aa2b378cf5a587
G1Joshi <joshijeevan98@gmail.com>
2022-01-21 18:27:44 +0530

    add utils

---
+++ lib/app/utils/helper/exception_handler.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+
+import '../../data/models/response/error_response.dart';
+import '../../data/values/strings.dart';
+
+class APIException implements Exception {
+  final String message;
+
+  APIException({required this.message});
+}
+
+class ExceptionHandler {
+  ExceptionHandler._();
+
+  static APIException handleError(Exception error) {
+    if (error is DioError) {
+      switch (error.type) {
+        case DioErrorType.sendTimeout:
+          return APIException(message: ErrorMessages.noInternet);
+        case DioErrorType.connectTimeout:
+          return APIException(message: ErrorMessages.connectionTimeout);
+        case DioErrorType.response:
+          return APIException(
+              message: ErrorResponse.fromJson(error.response?.data).message);
+        default:
+          return APIException(message: ErrorMessages.networkGeneral);
+      }
+    } else {
+      return APIException(message: ErrorMessages.networkGeneral);
+    }
+  }
+}
+
+class HandleError {
+  HandleError._();
+
+  static handleError(APIException? error) {
+    Get.rawSnackbar(message: error?.message ?? ErrorMessages.networkGeneral);
+  }
+}
